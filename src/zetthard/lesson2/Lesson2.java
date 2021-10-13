826cec38b07ae60911da5356ed094a670bd50e88
Semen Nikolaev <sr.nikolaev@gmail.com>
2021-10-13 21:37:42 +0300

    Lesson 2 exercises complete (with 2.5 and 2.6).

--- src/zetthard/lesson2/Lesson2.java
+++ src/zetthard/lesson2/Lesson2.java
@@ -1,21 +1,36 @@
 package zetthard.lesson2;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Lesson2 {
 
     public static void main(String[] args) {
 
+        //2.1 test
         //printOdd();
+
+        //2.2 test
         //fizzBuzzOption();
+
+        //2.3 test
         //System.out.println(checkSum());
+
+        //2.4 test
         //System.out.println(checkOrderAsc());
+
+        int[] arr1 = { 0, 3, 5, -3, 94, 3 };
+        System.out.println(Arrays.toString(arr1));
+        //2.5 test
+        //System.out.println(containsNumber(arr1, 3));
+        //2.6 test
+        //System.out.println(containsNumbers(arr1, 1, 3));
     }
 
     //2.1
     public static void printOdd() {
         for (int i = 0; i < 100; i++) {
-            if(i % 2 == 1) {
+            if (i % 2 == 1) {
                 System.out.println(i);
             }
         }
@@ -75,5 +90,27 @@ public class Lesson2 {
         num3 = sc.nextInt();
 
         return num3 > num2 && num2 > num1;
+    }
+
+    //2.5
+    public static boolean containsNumber(int[] arr, int val) {
+
+        return (arr[0] == val || arr[arr.length-1] == val);
+    }
+
+    //2.6
+    public static boolean containsNumbers(int[] arr, int val1, int val2 ) {
+
+        boolean res = false;
+        int[] vals = { val1, val2 };
+
+        for (int var :
+                vals) {
+            if (arr[0] == var || arr[arr.length - 1] == var) {
+                res = true;
+                break;
+            }
+        }
+        return res;
     }
 }
